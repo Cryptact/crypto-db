7a1b97b2cb075679844e348a3c8a211b9ddac176
smasuda <shunsuke.masuda@gmail.com>
2018-09-12 11:50:04 +0900

    update doc to refresh cache in circleci.

--- src/python/setup.py
+++ src/python/setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 setup(
     name='crypto-db',
     version='1.0.0',
-    description='Scripts for crypto-db',
+    description='Python scripts for crypto-db',
     url='https://github.com/cryptact/crypto-db',
     author='Shunsuke Masuda',
     license='MIT',
@@ -15,5 +15,5 @@ setup(
         'Programming Language :: Python :: 3.6'],
     packages=find_packages(exclude=['dev']),
     python_requires='>=3.6.3',
-    extras_require={'dev': ['pytest']},
+    extras_require={'dev': ['pytest']}
 )
