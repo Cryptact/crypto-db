ff1ab2815dc586357d7cd5515111369d418dec23
smasuda <shunsuke.masuda@gmail.com>
2018-09-13 14:58:31 +0900

    separate index.js to load

---
+++ crypto-db/composite/index.js
@@ -0,0 +1,14 @@
+/**
+ * exporting composite
+ *
+ * Copyright (C) 2018 Cryptact, LTD.
+ */
+
+// ls | sed -e "s/^/\"/g" -e "s/\.json/\",/g"
+const namespaces = ["cryptact"];
+
+module.exports = namespaces.reduce(
+    (all, ns) => (all[ns] = require("./" + ns)),
+    {}
+  );
+  
