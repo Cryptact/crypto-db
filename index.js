112c7ac4bac5571318efcc401c279e9a4342442a
smasuda <shunsuke.masuda@gmail.com>
2018-09-14 11:19:23 +0900

    folder name changes, rename jsons.

--- index.js
+++ index.js
@@ -3,14 +3,13 @@
  *
  * Copyright (C) 2018 Cryptact, LTD.
  */
-const crypto_db = require("./crypto-db/crypto-db");
-const crypto_actions = require("./crypto-db/crypto-actions");
+const crypto_db = require("./lib/ids");
+const crypto_actions = require("./lib/crypto-actions");
 
-const composite = require("./crypto-db/composite");
-const map_for_accuracy = require("./crypto-db/map_for_accuracy");
-const map_for_coverage = require("./crypto-db/map_for_coverage");
+const composite = require("./lib/composite");
+const map_for_accuracy = require("./lib/map_for_accuracy");
+const map_for_coverage = require("./lib/map_for_coverage");
 
-console.log("%j", map_for_accuracy);
 module.exports = {
   crypto_db,
   crypto_actions,
